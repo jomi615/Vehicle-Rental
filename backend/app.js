160c3d840011c4318de48dcc9aa10925c9ff4e8a
Canh Le <lecanhnhatminh@gmail.com>
2021-07-29 16:54:05 +0700

    added cert and run on https

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,30 @@
 const { response } = require("express");
 const express = require("express");
 var app = express();
+const https = require('https');
+const fs = require('fs');
+var path = require('path');
+var privateKey  = fs.readFileSync('backend/localhost-key.pem','utf8');
+var certificate = fs.readFileSync('backend/localhost.pem','utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+//session
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const oneDay = 1000 * 60 * 60 * 24;
+
+//session middleware
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -23,4 +44,6 @@ app.use('/', userReview);
 app.use('/', vehicleAdding);
 app.use('/', vehicleReview);
 app.use('/',sendMessaging )
-app.listen(4000);
+//app.listen(4000);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8000);
